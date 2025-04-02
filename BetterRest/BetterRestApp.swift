c5ab601323e21553f2fb5895d39458d66f324104
Gabriel Jesus Santos <84652898+bieeel9@users.noreply.github.com>
2025-04-01 22:38:02 -0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Gabriel Santos on 01/04/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
